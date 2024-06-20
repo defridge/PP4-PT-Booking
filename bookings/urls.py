0a2d6775a4c1d556b68275935804f77d822a1d31
Luke Masterson <info@herosgymtslc.com>
2024-06-20 18:37:26 +0000

    Created client bookings manage page for staff users in booking app

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('create/', views.create_booking, name='create_booking'),
     path('manage/', views.manage_bookings, name='manage_bookings'),
+    path('manage_clients/', views.manage_client_bookings, name='manage_client_bookings'),
     path('success/', views.booking_success, name='booking_success'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('edit_client/<int:booking_id>/', views.edit_client_booking, name='edit_client_booking'),
     path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('delete_client/<int:booking_id>/', views.delete_client_booking, name='delete_client_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from .forms import BookingForm
 from .models import Booking
+from django.utils import timezone
 
 
 @login_required
@@ -64,3 +65,37 @@ create_booking = custom_login_required_view(create_booking)
 manage_bookings = custom_login_required_view(manage_bookings)
 edit_booking = custom_login_required_view(edit_booking)
 delete_booking = custom_login_required_view(delete_booking)
+
+
+# Check if user is staff
+def is_staff(user):
+    return user.is_staff
+
+@login_required
+@user_passes_test(is_staff)
+def manage_client_bookings(request):
+    today = timezone.now().date()
+    bookings = Booking.objects.filter(date__gte=today).order_by('date', 'time')
+    return render(request, 'bookings/manage_client_bookings.html', {'bookings': bookings})
+
+@login_required
+@user_passes_test(is_staff)
+def edit_client_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('manage_client_bookings')
+    else:
+        form = BookingForm(instance=booking)
+    return render(request, 'bookings/edit_client_booking.html', {'form': form})
+
+@login_required
+@user_passes_test(is_staff)
+def delete_client_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('manage_client_bookings')
+    return render(request, 'bookings/delete_client_booking.html', {'booking': booking})
